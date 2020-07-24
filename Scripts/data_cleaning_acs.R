02d8a72ae16602ecca114fe3ee812b0f6ffbdea6
DarrinWeber <weber.darrin@gmail.com>
2020-07-24 15:45:16 -0500

    Cleaned ACS Education data
    *added code to clean the ACS Educational Attainment data. Also added code to automatically read in NA values from the CSV files. Also, added code to drop columns with all NA values

--- Scripts/data_cleaning_acs.R
+++ Scripts/data_cleaning_acs.R
@@ -4,7 +4,8 @@ library(tidyverse)
 
 # Read in ACS Demographic/Housing Data
 acs_demo <- read_csv("Data/ACS_Demographic_and_Housing_Estimates/ACSDP5Y2018.DP05_data_with_overlays_2020-07-23T014728.csv",
-                     skip = 1)
+                     skip = 1,
+                     na = c("*****","-"))
 
 # Select only the relevant columns
 acs_demo_1 <- acs_demo %>% select(id, `Geographic Area Name`, starts_with("Estimate")) 
@@ -19,4 +20,20 @@ acs_demo_2 <- acs_demo_1[, str_count(colnames(acs_demo_1), "!!") <= 4] %>%
   select(-(36:45)) %>% 
   separate(`Geographic Area Name`, into = c("County", "State"), sep = ",")
 
+# Drop columns with all NA
+acs_demo_3 <- acs_demo_2[, !map_lgl(acs_demo_2, ~ all(is.na(.))), drop = FALSE]
 
+
+# Read in ACS Education Data
+acs_edu <- read_csv("Data/ACS_Educational_Attainment/ACSST5Y2018.S1501_data_with_overlays_2020-07-23T004821.csv",
+                    skip = 1,
+                    na = c("(X)","-"))
+
+# Select only the relevant columns
+acs_edu_1 <- acs_edu %>% select(id, `Geographic Area Name`, starts_with("Estimate"))
+
+acs_edu_2 <- acs_edu_1 %>% select(-contains(c("Percent",
+                                              "Total"))) 
+
+# Drop columsn with all NA
+acs_edu_3 <- acs_edu_2[, !map_lgl(acs_edu_2, ~ all(is.na(.))), drop = FALSE]
