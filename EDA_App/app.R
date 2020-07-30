1b71fc9d30073e43976d525bac1cb8ba7d55cc38
DarrinWeber <weber.darrin@gmail.com>
2020-07-29 19:23:22 -0500

    Created plots for income and education
    *Imported IN_edu dataset. Also created the bar plots for income level and education level for any county in Indiana.

--- EDA_App/app.R
+++ EDA_App/app.R
@@ -5,7 +5,24 @@
 library(shiny)
 library(tidyverse)
 
-IN_income <- read_csv("../Data/IN_income.csv")
+IN_income <- read_csv("../Data/IN_income.csv",
+                      col_types = cols(
+                          County = col_character(),
+                          State = col_character(),
+                          variable = col_character(),
+                          estimate = col_double(),
+                          label = col_factor()
+                      ))
+IN_edu <- read_csv("../Data/IN_edu.csv",
+                   col_types = cols(
+                       County = col_character(),
+                       State = col_character(),
+                       variable = col_character(),
+                       estimate = col_double(),
+                       Sex = col_factor(),
+                       Age = col_factor(),
+                       Education = col_factor()
+                   ))
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -24,7 +41,8 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("income_plot"),
+           plotOutput("edu_plot")
         )
     )
 )
@@ -46,6 +64,30 @@ server <- function(input, output) {
         selectInput("county",
                     "County",
                     county)
+    })
+    
+    output$income_plot <- renderPlot({
+        ggplot(filter(IN_income, 
+                      State == input$state,
+                      County == input$county)) + 
+            geom_col(aes(x = label, y = estimate)) +
+            labs(x = "Family Income", 
+                 y = "Number of People",
+                 title = str_c("Distribution of Income in ", input$county, " County, ", input$state)) +
+            theme(axis.text.x = element_text(angle = 45,
+                                             hjust = 1))
+    })
+    
+    output$edu_plot <- renderPlot({
+        ggplot(filter(IN_edu, 
+                      State == input$state,
+                      County == input$county)) + 
+            geom_col(aes(x = Education, y = estimate)) +
+            labs(x = "Level of Education", 
+                 y = "Number of People",
+                 title = str_c("Educational Attainment in ", input$county, " County, ", input$state)) +
+            theme(axis.text.x = element_text(angle = 45,
+                                             hjust = 1))
     })
 }
 
