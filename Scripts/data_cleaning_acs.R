f3bb9014ca616f6f8a464f1c140495a7634e2156
DarrinWeber <weber.darrin@gmail.com>
2020-08-04 14:01:59 -0500

    Cleaned USA Facts Confirmed cases data
    *Got COVID cases data ready for importation into EDA_App

--- Scripts/data_cleaning_acs.R
+++ Scripts/data_cleaning_acs.R
@@ -16,6 +16,10 @@ source("Scripts/functions.R")
 #variables_2018 <- load_variables(2018, "acs5", cache = TRUE) %>% 
 #  rename(variable = name)
 
+# This creates a reference database to convert between the 
+# name of a state and its abbreviation
+state_abb_to_name <- tibble(State = state.name, Abb = state.abb)
+
 
 ######################################
 # Retrieve Income data
@@ -179,12 +183,24 @@ IN_gini$label <- str_remove(IN_gini$label, "Estimate!!")
 write_csv(IN_gini, "Data/IN_gini.csv")
 
 
+######################################
+# Clean USA Facts Confirmed data
+######################################
 
+covid_conf <- read_csv("Data/usafacts_covid_confirmed.csv")
 
+covid_conf <- covid_conf %>% select(`County Name`, State, tail(names(.),1)) %>% 
+  rename_at(vars(tail(names(.),1)), funs(c("Cases")))
 
+covid_conf <- left_join(covid_conf, state_abb_to_name, 
+                             by = c("State" = "Abb")) %>% 
+  select(!State) %>% 
+  rename(State = State.y,
+         County = `County Name`) %>% 
+  mutate(NAME = str_c(County, State, sep = ", ")) %>% 
+  select(NAME, Cases)
 
-
-
+write_csv(covid_conf, "Data/covid_cases.csv")
 
 
 
