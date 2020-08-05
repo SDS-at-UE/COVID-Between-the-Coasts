23c27eb84767ee0c294ef42ff3c115cf0fe7355f
DarrinWeber <weber.darrin@gmail.com>
2020-08-05 14:45:41 -0500

    Moved USA Facts Confirmed COVID data to own script
    *This data cleaning is now in data_cleaning_covid.R

--- Scripts/data_cleaning_acs.R
+++ Scripts/data_cleaning_acs.R
@@ -183,24 +183,6 @@ IN_gini$label <- str_remove(IN_gini$label, "Estimate!!")
 write_csv(IN_gini, "Data/IN_gini.csv")
 
 
-######################################
-# Clean USA Facts Confirmed data
-######################################
-
-covid_conf <- read_csv("Data/usafacts_covid_confirmed.csv")
-
-covid_conf <- covid_conf %>% select(`County Name`, State, tail(names(.),1)) %>% 
-  rename_at(vars(tail(names(.),1)), funs(c("Cases")))
-
-covid_conf <- left_join(covid_conf, state_abb_to_name, 
-                             by = c("State" = "Abb")) %>% 
-  select(!State) %>% 
-  rename(State = State.y,
-         County = `County Name`) %>% 
-  mutate(NAME = str_c(County, State, sep = ", ")) %>% 
-  select(NAME, Cases)
-
-write_csv(covid_conf, "Data/covid_cases.csv")
 
 
 
