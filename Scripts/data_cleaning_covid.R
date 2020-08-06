3019cb8b81e19cc5755455459f23dc3e274c269e
DarrinWeber <weber.darrin@gmail.com>
2020-08-06 10:39:23 -0500

    Cleaned death and population USA Facts data
    *Cleaned and combined death, confirmed cases, and population data from USA Facts. Also filtered the results to include just the relevant states to the investigation.

--- Scripts/data_cleaning_covid.R
+++ Scripts/data_cleaning_covid.R
@@ -7,24 +7,70 @@ source("Scripts/functions.R")
 
 
 
-
-
-
 ######################################
-# Clean USA Facts Confirmed data
+# Clean USA Facts data
 ######################################
+
+### #####################
+### Clean Confirmed Cases data
+### #####################
 
 covid_conf <- read_csv("Data/usafacts_covid_confirmed.csv")
 
 covid_conf <- covid_conf %>% select(`County Name`, State, tail(names(.),1)) %>% 
-  rename_at(vars(tail(names(.),1)), funs(c("Cases")))
+  rename_at(vars(tail(names(.),1)), funs(c("Cases"))) %>% 
+  filter(State %in% c("IN", "IL", "KY", "OH", "MI", "MN", "WI"))
 
 covid_conf <- left_join(covid_conf, state_abb_to_name, 
                         by = c("State" = "Abb")) %>% 
-  select(!State) %>% 
-  rename(State = State.y,
+  rename(State_abb = State,
+         State = State.y,
          County = `County Name`) %>% 
-  mutate(NAME = str_c(County, State, sep = ", ")) %>% 
-  select(NAME, Cases)
+  mutate(NAME = str_c(County, State, sep = ", "))
 
-write_csv(covid_conf, "Data/covid_cases.csv")
+
+### #####################
+### Clean Population data
+### #####################
+
+county_pop <- read_csv("Data/usafacts_covid_county_population.csv")
+
+county_pop <- county_pop %>% select(`County Name`, State, tail(names(.),1)) %>% 
+  rename_at(vars(tail(names(.),1)), funs(c("Population"))) %>% 
+  filter(State %in% c("IN", "IL", "KY", "OH", "MI", "MN", "WI"))
+
+county_pop <- left_join(county_pop, state_abb_to_name, 
+                        by = c("State" = "Abb")) %>%
+  rename(State_abb = State,
+         State = State.y,
+         County = `County Name`) %>% 
+  mutate(NAME = str_c(County, State, sep = ", "))
+
+### #####################
+### Clean Deaths data
+### #####################
+
+covid_death <- read_csv("Data/usafacts_deaths.csv")
+
+covid_death <- covid_death %>% select(`County Name`, State, tail(names(.),1)) %>% 
+  rename_at(vars(tail(names(.),1)), funs(c("Deaths"))) %>% 
+  filter(State %in% c("IN", "IL", "KY", "OH", "MI", "MN", "WI"))
+
+covid_death <- left_join(covid_death, state_abb_to_name, 
+                         by = c("State" = "Abb")) %>%
+  rename(State_abb = State,
+         State = State.y,
+         County = `County Name`) %>% 
+  mutate(NAME = str_c(County, State, sep = ", "))
+
+### #####################
+### Combine Datasets to create master USA Facts data
+### #####################
+
+usafacts_covid <- left_join(covid_conf, covid_death, by = c("NAME", "County", "State", "State_abb")) %>% 
+  left_join(county_pop, by = c("NAME", "County", "State", "State_abb")) %>%
+  select(NAME, County, State, State_abb, everything())
+
+
+
+
