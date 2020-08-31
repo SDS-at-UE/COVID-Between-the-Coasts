ddb206eafa29f6b1d49dfed8d3ee11ac5e48cf8e
DarrinWeber <weber.darrin@gmail.com>
2020-08-31 11:35:22 -0500

    Created chicago_eda.R. Downloaded zip code data for Direct-Purchase Health Insurance by Sex by Age.

---
+++ Scripts/chicago_eda.R
@@ -0,0 +1,11 @@
+# Chicago EDA
+## Exploring demographic data for Chicago
+
+library(tidycensus)
+library(tidyverse)
+
+chicago_data <- get_acs(geography = "zcta",
+                        year = 2018,
+                        table = "C27005",
+                        geometry = TRUE)
+chicago_data <- left_join(chicago_data, variables_2018[, 1:2], by = "variable")
