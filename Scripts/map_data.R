4ba5986db62c912dbe779979e0308590121309ea
DarrinWeber <weber.darrin@gmail.com>
2020-09-02 01:53:43 -0500

    Added code obtaining geometry from Census bureau for both counties and zip codes

--- Scripts/map_data.R
+++ Scripts/map_data.R
@@ -10,3 +10,21 @@ zip_data <- read_csv("C:/Users/Darrin Weber/Desktop/zip_data.csv",
 
 zip_data <- zip_data %>% select(zip, type, primary_city, acceptable_cities, state, county, latitude, longitude)
 write_csv(zip_data, "Data/zip_county.csv")
+
+
+# Obtain geometry data from ACS survey for mapping purposes
+# to create All_counties.shp
+map_data <- get_acs(geography = "county",
+                    variables = "B25077_001",
+                    state = c("IN", "IL", "KY", "OH", "MI", "MN", "WI"),
+                    year = 2018,
+                    geometry = TRUE)
+
+geometry_export <- select(map_data, NAME, geometry)
+write_sf(geometry_export, "Data/All_counties.shp")
+
+# Obtain geometry data from ACS survey for ZIP codes.
+zip_map_data <- get_acs(geography = "zcta",
+                        variables = "B25077_001",
+                        year = 2018,
+                        geometry = TRUE)
