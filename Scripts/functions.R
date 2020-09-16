ae571493ddacaaa106a36fe6a7049bfb4db8d212
mayafrederick <mf203@evansville.edu>
2020-09-16 12:17:51 -0400

    Test change.

--- Scripts/functions.R
+++ Scripts/functions.R
@@ -9,6 +9,7 @@ library(stringr)
 library(sf)
 library(tigris)
 
+# University of Evansville Purple Aces
 
 ### This loads the necessary variable-to-table data to connect table
 ### numbers in the Census dataset to their actual meanings. It should
