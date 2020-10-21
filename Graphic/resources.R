19197d642082ba5ebc80b8515830024c812d366e
DarrinWeber <weber.darrin@gmail.com>
2020-10-21 06:28:08 -0500

    Created a resources file to store helpful resources we might need to use for creation of the graphic.

---
+++ Graphic/resources.R
@@ -0,0 +1,23 @@
+## Resources for Graphic
+### below are some resources to help build
+### our graphic
+
+# The basic part of a Shiny app
+# https://shiny.rstudio.com/articles/basics.html
+
+# How to build a Shiny app
+# https://shiny.rstudio.com/articles/build.html
+
+# Gallery of Shiny App Examples
+# https://shiny.rstudio.com/gallery/
+
+# Intro to gganimate
+# https://gganimate.com/articles/gganimate.html
+### This website talks about the gganimate package
+### which can turn regular ggplot graphics into
+### an animation. I'm not sure this will be used
+### but I'll still leave it here just in case
+
+# Another quick example of a leaflet timeline
+# http://boazsobrado.com/blog/2018/02/08/leaflet-timeline-in-r/
+
