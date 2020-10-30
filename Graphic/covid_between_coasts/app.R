c296effd79f0517b3d1aa8666b3b402cebe7d82e
EthanMorlock <ethan.morlock98@gmail.com>
2020-10-30 11:57:04 -0500

    Added leaflet to UI and made the sidebar panel and layout

--- Graphic/covid_between_coasts/app.R
+++ Graphic/covid_between_coasts/app.R
@@ -56,17 +56,25 @@ ui <- fluidPage(
   # Application title
   titlePanel("COVID Between the Coasts"),
   
-  selectInput(inputId = "states", "Choose a State", c("All", "Kentucky", "Illinois", "Indiana", "Michigan", "Minnesota", "Ohio", "Wisconsin")),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "states", "Choose a State", c("All", "Kentucky", "Illinois", "Indiana", "Michigan", "Minnesota", "Ohio", "Wisconsin")),
   
-  radioButtons(inputId = "stat", "Choose a Statistic", c("Total Cases", "Total Deaths", "Case Rate per 100,000", 
+      radioButtons(inputId = "stat", "Choose a Statistic", c("Total Cases", "Total Deaths", "Case Rate per 100,000", 
                                                          "Death Rate per 100,000", "Case Fatality Rate", "7 Day Moving Average")),
   
-  sliderInput(inputId = "dates", "Timeline of COVID", 
+      sliderInput(inputId = "dates", "Timeline of COVID", 
               min = as.Date("01-01-2020","%m-%d-%Y"),
               max = as.Date("10-31-2020","%m-%d-%Y"),
-              value=as.Date("06-24-2020","%m-%d-%Y")),
+              value=as.Date("06-24-2020","%m-%d-%Y"),
+              animate = TRUE),
   
-  dateInput(inputId = "date_input", "Type in date you want to see", value = as.Date("06-24-2020","%m-%d-%Y"), format = "mm-dd-yyyy") 
+      dateInput(inputId = "date_input", "Type in date you want to see", value = as.Date("06-24-2020","%m-%d-%Y"), format = "mm-dd-yyyy") 
+),
+
+  mainPanel(
+    leafletOutput("map_cases")
+  ))
 )
 
 
