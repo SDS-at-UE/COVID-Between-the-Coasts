0825dc49d8ca43fc81ba4c46c7679584c30d4a55
mayafrederick <mf203@evansville.edu>
2020-11-04 12:25:14 -0500

    Fixed variable names and wrote it to a csv file.

--- Graphic/statewide_unallocated_data.R
+++ Graphic/statewide_unallocated_data.R
@@ -9,5 +9,10 @@ cases <- cases[,-c(1,4)]
 cases <- cases %>% filter(str_detect(`County Name`, "Statewide Unallocated"))
 cases <- cases %>% filter(State %in% c("IN", "KY", "MI", "OH", "IL", "WI", "MN"))
 
-cases <- cases %>% 
+statewide_unallocated <- cases %>% 
   pivot_longer(!c(`County Name`, State), names_to = "Date", values_to = "cases")
+
+statewide_unallocated <- statewide_unallocated %>% rename(county_name = `County Name`)
+
+
+write_csv(statewide_unallocated, "Data/graphic_covid.csv")
