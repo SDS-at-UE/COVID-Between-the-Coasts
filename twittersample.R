34d02376e38bc42a10b0ecf55cd09633f3419351
timmymiller1 <tm206@evansville.edu>
2020-11-15 22:51:07 -0600

    changed everything to stringr commands

--- twittersample.R
+++ twittersample.R
@@ -1,8 +1,10 @@
 install.packages("twitteR")
+library(tidyverse)
 library(twitteR)
 library(openssl)
 library(httpuv)
 library(base64enc)
+libray(readr)
 setup_twitter_oauth( consumer_key="6WoyE8SKPd42xyX8cJA7PAAEo",
                      consumer_secret="vBeKwvD8AvsGlAvSUZyZSCuquuwBja7VowOg0KtNVLYGVptIsL",
                      access_token = "1327467001850322944-tv4VeJdEAnMPZ8YnhxKDWr5FDeUzyM",
@@ -13,6 +15,7 @@ updateStatus(tweetfinal)
 
 ####new cases for 2020-11-01
 
+graphic_covid <- read_csv("Data/graphic_covid.csv")
 graphic_covid$date<- graphic_covid$date %>% as.character()
 new_cases_for_tweet<- filter(graphic_covid, date=="2020-11-02")
 new_cases_for_tweet<- new_cases_for_tweet %>% select(date, state, new_cases)
@@ -21,6 +24,6 @@ new_cases_for_tweet$`sum(new_cases)`<- new_cases_for_tweet$`sum(new_cases)` %>%
                                       as.character()
 dailycases<-str_c(new_cases_for_tweet$`sum(new_cases)`, sep=",")
 states<- str_c(new_cases_for_tweet$state, sep=",")
-tweet<- paste(states, dailycases, sep=":")
+tweet<- str_c( states, dailycases, sep=":")
 tweetfinal<- str_c(tweet, collapse=", ")
 updateStatus(tweetfinal)
