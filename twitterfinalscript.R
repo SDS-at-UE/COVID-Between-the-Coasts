4fd9b2f56a76fbd4c172128fa7b2c4432f96dbd1
timmymiller1 <tm206@evansville.edu>
2020-11-23 13:05:04 -0600

    added rolling average

--- twitterfinalscript.R
+++ twitterfinalscript.R
@@ -110,6 +110,10 @@ state_abb_to_name <- tibble(State = state.name, Abb = state.abb)
 #Left joining covid and state names by their abbreviations
 covid_data <- left_join(final_covid, state_abb_to_name, by = c("state"= "Abb"))
 
+###tweet info
+
+##cases tweet
+
 covid_data_for_tweets<- covid_data %>% select(date, state, new_cases) %>%
   group_by(date, state) %>% summarize(state_new_cases=sum(new_cases))
 
@@ -127,5 +131,25 @@ intro_to_tweet<- str_c("Daily Newly Reported Cases by State for ",
 tweetfinal<-str_c(intro_to_tweet, sep="\n\n", tweetwithspace)
 
 post_tweet(tweetfinal)
+
+#rolling average tweet
+
+covid_data_for_tweets_rollingavg<- covid_data %>% select(date, state, moving_7_day_avg) %>%
+  group_by(date, state) %>% summarize(state_rollingavg=round(sum(moving_7_day_avg)))
+
+manual_tweet_data_rollingavg<- covid_data_for_tweets_rollingavg %>% 
+  filter(date==max(covid_data_for_tweets_rollingavg$date))
+
+display_date<-stamp_date("January 22, 2020")
+dailyrollingavg<-str_c(manual_tweet_data_rollingavg$state_rollingavg, sep=",")
+states_rollingavg<- str_c(manual_tweet_data_rollingavg$state, sep=",")
+tweet_initial_rollingavg<- str_c( states_rollingavg, dailyrollingavg, sep=": ")
+tweetwithspace_rollingavg<- str_c(tweet_initial_rollingavg, collapse="\n")
+intro_to_tweet_rollingavg<- str_c("Daily Case Rolling Average by State for ", 
+                                  display_date(max(covid_data_for_tweets_rollingavg$date)-6), " through ",
+                       display_date(max(covid_data_for_tweets_rollingavg$date)))
+tweetfinal_rollingavg<-str_c(intro_to_tweet_rollingavg, sep="\n\n", tweetwithspace_rollingavg)
+
+post_tweet(tweetfinal_rollingavg)
 
 
